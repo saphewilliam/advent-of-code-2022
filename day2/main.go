c83375d999850ac70165a35c7f3129b487587dd7
William Ford <william@saphewebdesign.com>
2022-12-07 15:38:42 +0100

    Simplify day 2 formulas a bit

--- day2/main.go
+++ day2/main.go
@@ -4,9 +4,9 @@ import "aoc-2022/lib"
 
 func Process(input []string) (score1 lib.Solution, score2 lib.Solution) {
 	for _, v := range input {
-		a, b := int(v[0])-65, int(v[2])-88
-		score1.I += 1 + b + 3*((4+b-a)%3)
-		score2.I += 1 + (a+b+2)%3 + b*3
+		a, b := int(v[0])-65, int(v[2])-84
+		score1.I += b + 3*((b-a)%3-1)
+		score2.I += (a+b-2)%3 + b*3 - 11
 	}
 	return
 }
