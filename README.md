4adee691990eea62c43704eaa8a5c481c5986d7e
William Ford <william@saphewebdesign.com>
2022-12-09 10:29:06 +0100

    Add testOnly flag to main script

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Advent of Code 2022
 
-To run the code of a (set of) day(s), run `make D="<space delimited string of day numbers>"`.
+To run the code of a (set of) day(s), run `make D="<space delimited string of day numbers>"`. To only run the test inputs for those days, add a 0 to the days argument.
 
-For example: `make D=3` runs the solution code for day 3, `make D="1 2 5"` runs the solution code for days 1, 2, and 5.
+For example: 
+- `make D=3` runs the solution code for day 3
+- `make D="1 2 5"` runs the solution code for days 1, 2, and 5
+- `make D="0 2 4"` runs the solution code for only the test inputs of days 2 and 4.
 
 https://adventofcode.com/2022
--- main.go
+++ main.go
@@ -33,9 +33,12 @@ func gradeSolution(name, correct string, test lib.Solution) {
 	fmt.Printf("%v: %v\n", name, test.GetValue())
 }
 
-// TODO testonly/realonly flag
 func main() {
-	for _, v := range os.Args[1:] {
+	args, testOnly := os.Args[1:], false
+	if args[0] == "0" {
+		args, testOnly = args[1:], true
+	}
+	for _, v := range args {
 		day, dayFound := getDay(v)
 		if !dayFound {
 			fmt.Println(v + " is not a valid day value!")
@@ -44,16 +47,17 @@ func main() {
 		testInput := readLines("./day" + v + "/testinput.txt")
 		input := readLines("./day" + v + "/input.txt")
 
+		fmt.Println("Day", v)
 		timeStart := time.Now()
 		testSolution1, testSolution2 := day.Process(testInput)
-		solution1, solution2 := day.Process(input)
-		timeElapsed := time.Since(timeStart)
-
-		fmt.Println("Day", v)
 		gradeSolution("Test solution 1", day.TestPart1, testSolution1)
 		gradeSolution("Test solution 2", day.TestPart2, testSolution2)
-		gradeSolution("Solution 1", day.Part1, solution1)
-		gradeSolution("Solution 2", day.Part2, solution2)
+		if !testOnly {
+			solution1, solution2 := day.Process(input)
+			gradeSolution("Solution 1", day.Part1, solution1)
+			gradeSolution("Solution 2", day.Part2, solution2)
+		}
+		timeElapsed := time.Since(timeStart)
 		fmt.Printf("Time elapsed: %dms\n\n", timeElapsed.Milliseconds())
 	}
 }
