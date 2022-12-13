e490a29e0131bd49b3a751bfa22e3a4b1973d08b
William Ford <william@saphewebdesign.com>
2022-12-13 13:00:46 +0100

    Day 12

--- day12/main.go
+++ day12/main.go
@@ -1,7 +1,71 @@
 package day12
 
-import "aoc-2022/lib"
+import (
+	"aoc-2022/lib"
+)
+
+func bfs(points []rune, width, start, goal int) (parents []int) {
+	q := lib.NewQueue(start)
+	for range points {
+		parents = append(parents, -1)
+	}
+
+	for !q.IsEmpty() {
+		p := q.Dequeue()
+		if p == goal {
+			return
+		}
+
+		evaluateDirection := func(pDir int, cond bool) {
+			if cond && pDir != start && parents[pDir] == -1 && points[pDir] <= points[p]+1 {
+				parents[pDir] = p
+				q.Enqueue(pDir)
+			}
+		}
+
+		evaluateDirection(p-width, p >= width)            // up
+		evaluateDirection(p+width, p+width < len(points)) // down
+		evaluateDirection(p-1, p%width != 0)              // left
+		evaluateDirection(p+1, p%width != width-1)        // right
+	}
+	return
+}
+
+func getDistance(points []rune, width, start, goal int) (dist int) {
+	parents := bfs(points, width, start, goal)
+	for ; goal != start && parents[goal] != -1; dist++ {
+		goal = parents[goal]
+	}
+	return
+}
 
 func Process(input []string) (solution1 lib.Solution, solution2 lib.Solution) {
+	points, starts, goal := []rune{}, []int{}, -1
+	for _, row := range input {
+		for _, v := range row {
+			switch v {
+			case 'a':
+				starts = append(starts, len(points))
+			case 'S':
+				v = 'a'
+				starts = append([]int{len(points)}, starts...)
+			case 'E':
+				v = 'z'
+				goal = len(points)
+			}
+			points = append(points, v)
+		}
+	}
+
+	for i, s := range starts {
+		dist := getDistance(points, len(input[0]), s, goal)
+		if i == 0 {
+			solution1.I = dist
+			solution2.I = dist
+		}
+		if dist > 0 && dist < solution2.I {
+			solution2.I = dist
+		}
+	}
 	return
 }
